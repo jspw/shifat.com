183c2e1e129b773c42f0d5b0f27fcc6a49fad138
mh-shifat <mhshifat757@gmail.com>
2020-06-23 18:25:07 +0600

    new style added

---
+++ blog/static/js/blog.js
@@ -0,0 +1,15 @@
+var offsetHeight = document.getElementById('main').offsetHeight;
+console.log(offsetHeight);
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
+
+  
