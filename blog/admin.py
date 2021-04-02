e4984ab5ae7ce2a6ad4d0f71496a7c21851b5db0
jspw <mhshifat757@gmail.com>
2021-04-02 10:11:59 +0600

    portfolio redesigned by react

--- blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from blog.models import Post,Comment
-# Register your models here.
-
-
-admin.site.register(Post)
-admin.site.register(Comment)
---
+++ src/container/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "../components/Feed/About";
+import NavBar from "../components/NavBar/NavBar";
+import "./App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <NavBar />
+
+      <Switch>
+        <Route path="/">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
